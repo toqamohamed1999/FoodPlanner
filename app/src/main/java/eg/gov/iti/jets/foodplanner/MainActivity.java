261b23dd6fccf641e529ebeef4e2d3bda123e2e4
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-01-30 21:41:53 +0200

    Connect android project to github

---
+++ app/src/main/java/eg/gov/iti/jets/foodplanner/MainActivity.java
@@ -0,0 +1,14 @@
+package eg.gov.iti.jets.foodplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

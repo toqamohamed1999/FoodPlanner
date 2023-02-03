1bd80bd5e33f9ec5c0942d937eb5d4abb68c1ac1
Monica <hahaha@hehehe.com>
2023-02-03 18:33:28 +0200

    bottom navigation bar with search&home&plane and favourites fragments

---
+++ app/src/main/java/eg/gov/iti/jets/foodplanner/Meal_Details_Activity.java
@@ -0,0 +1,14 @@
+package eg.gov.iti.jets.foodplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meal_Details_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_details);
+    }
+}
---
+++ app/src/main/java/eg/gov/iti/jets/foodplanner/SearchFragment.java
@@ -0,0 +1,30 @@
+package eg.gov.iti.jets.foodplanner;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class SearchFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}

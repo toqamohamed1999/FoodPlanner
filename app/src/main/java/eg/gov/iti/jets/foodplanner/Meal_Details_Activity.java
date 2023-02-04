8cfff70b749c80e697074e95d8c11e405e02a7e8
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-02-04 23:58:33 +0200

    fix conflict

--- app/src/main/java/eg/gov/iti/jets/foodplanner/Meal_Details_Activity.java
+++ app/src/main/java/eg/gov/iti/jets/foodplanner/Meal_Details_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OnSearchingActivity extends AppCompatActivity {
+public class Meal_Details_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_on_searching);
+        setContentView(R.layout.activity_meal_details);
     }
 }

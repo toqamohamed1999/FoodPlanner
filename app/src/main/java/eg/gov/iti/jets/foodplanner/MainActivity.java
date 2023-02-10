3b2799db9c425bccdff83778699732fb51918292
toqamohamed1999 <toqa111999mohamed@gmail.com>
2023-02-11 00:22:50 +0200

    backup meals to firebase and restore meals from firebase

--- app/src/main/java/eg/gov/iti/jets/foodplanner/MainActivity.java
+++ app/src/main/java/eg/gov/iti/jets/foodplanner/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController= Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
     }
 
 }
